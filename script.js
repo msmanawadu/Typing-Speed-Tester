4f57ff521156ba28190cfbb2cb6c3eaf9f3815bb
Sandeepa Manawadu <msmanawadu@gmail.com>
2020-11-06 09:01:49 +0530

    use event listeners to detect typing

--- script.js
+++ script.js
@@ -12,12 +12,22 @@ const theTimer = document.querySelector(".timer");
 
 
 // Match the text entered with the provided text on the page:
-
+function spellCheck(){
+    let textEntered = testArea.value;
+}
 
 // Start the timer:
+function start(){
+    let textEnteredLength = testArea.value.length;
 
+}
 
 // Reset everything:
-
+function reset(){
+    console.log("Timer was resetted !")
+}
 
 // Event listeners for keyboard input and the reset button:
+testArea.addEventListener("keypress", start, false);
+testArea.addEventListener("keyup", spellCheck, false);
+resetButton.addEventListener("click", reset, false);
