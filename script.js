f310f5f97ed1520292435dad43874e3a5c7d7824
Sandeepa Manawadu <msmanawadu@gmail.com>
2020-11-06 09:48:11 +0530

    build the count-up timer

--- script.js
+++ script.js
@@ -4,12 +4,22 @@ const originText = document.querySelector("#origin-text p").innerHTML;
 const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
+var timer = [0,0,0,0];
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
 
 
 // Run a standard minute/second/hundredths timer:
+function runTimer(){
+    let currentTime = timer[0] + ":" + timer[1] + ":" + timer[2];
+    theTimer.innerHTML = currentTime;
+    timer[3]++;
 
+    // Math calculation
+    timer[0] = Math.floor((timer[3]/100)/60);
+    timer[1] = Math.floor((timer[3]/100) - (timer[0] * 60));
+    timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0] * 6000));
+}
 
 // Match the text entered with the provided text on the page:
 function spellCheck(){
@@ -19,6 +29,9 @@ function spellCheck(){
 // Start the timer:
 function start(){
     let textEnteredLength = testArea.value.length;
+    if(textEnteredLength === 0){
+        setInterval(runTimer, 10);
+    }
 
 }
 
