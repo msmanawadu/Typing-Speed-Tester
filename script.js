7d16b2511e316a093f8b2ec9561a3a68e0d442d9
Sandeepa Manawadu <msmanawadu@gmail.com>
2020-11-06 10:06:54 +0530

    added a helper function for leading zeros

--- script.js
+++ script.js
@@ -6,12 +6,17 @@ const theTimer = document.querySelector(".timer");
 
 var timer = [0,0,0,0];
 
-// Add leading zero to numbers 9 or below (purely for aesthetics):
-
+// Add leading zero to 0-9 numbers (using helper function to augment content of runTimer()):
+function leadingZero(time){
+    if(time <= 9){
+        time = "0" + time;
+    }
+    return time;
+}
 
 // Run a standard minute/second/hundredths timer:
 function runTimer(){
-    let currentTime = timer[0] + ":" + timer[1] + ":" + timer[2];
+    let currentTime = leadingZero(timer[0]) + ":" + leadingZero(timer[1]) + ":" + leadingZero(timer[2]);
     theTimer.innerHTML = currentTime;
     timer[3]++;
 
